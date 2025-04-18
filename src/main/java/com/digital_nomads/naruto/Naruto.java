4e7cb58f0909f5fefe582447523f93302589cd95
Almazbek Tynybekov <almazbek.tynybekov@timelysoft.net>
2025-04-18 15:40:17 +0600

    naruto commit

---
+++ src/main/java/com/digital_nomads/naruto/Naruto.java
@@ -0,0 +1,8 @@
+package com.digital_nomads.naruto;
+
+public class Naruto {
+    public static void main(String[] args) {
+        System.out.println("Uzumaki Naruto");
+        System.out.println("From Konoha village");
+    }
+}
---
+++ src/main/java/com/digital_nomads/naruto/bio.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.naruto;
+
+public class bio {
+    public static void main(String[] args) {
+        System.out.println("Naruto Uzumaki age - 16");
+    }
+}
