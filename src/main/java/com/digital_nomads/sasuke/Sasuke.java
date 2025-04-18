3ec5583502f63941d1b9c7a06b1bcd9a07493364
Almazbek Tynybekov <almazbek.tynybekov@timelysoft.net>
2025-04-18 15:35:44 +0600

    sasuke commit

---
+++ src/main/java/com/digital_nomads/sasuke/Sasuke.java
@@ -0,0 +1,8 @@
+package com.digital_nomads.sasuke;
+
+public class Sasuke {
+    public static void main(String[] args) {
+        System.out.println("Uchiha Sasuke");
+        System.out.println("from Uchiha clan");
+    }
+}
