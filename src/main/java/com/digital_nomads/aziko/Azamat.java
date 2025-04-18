dcd3fd523f0ccf5623c0c6948dd3d96a9677f08c
Aziko050050 <48541880+yakuza050050@users.noreply.github.com>
2025-04-18 15:47:59 +0600

    Update Azamat.java

--- src/main/java/com/digital_nomads/aziko/Azamat.java
+++ src/main/java/com/digital_nomads/aziko/Azamat.java
@@ -2,7 +2,8 @@ package com.digital_nomads.aziko;
 
 public class Azamat {
     public static void main(String[] args) {
-        System.out.println("Azamat");
+        System.out.println("Azamat Tynybekov");
         System.out.println("from Kyrgyzstan");
+         System.out.println("love is Manhwa");
     }
 }
