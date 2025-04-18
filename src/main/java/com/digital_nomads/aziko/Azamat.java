38848eacd48a0fd38c9ab6a54887877628dcfb63
Almazbek Tynybekov <almazbek.tynybekov@timelysoft.net>
2025-04-18 15:32:56 +0600

    commit azamat

---
+++ src/main/java/com/digital_nomads/aziko/Azamat.java
@@ -0,0 +1,8 @@
+package com.digital_nomads.aziko;
+
+public class Azamat {
+    public static void main(String[] args) {
+        System.out.println("Azamat");
+        System.out.println("from Kyrgyzstan");
+    }
+}
---
+++ src/main/java/com/digital_nomads/aziko/Bio.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.aziko;
+
+public class Bio {
+    public static void main(String[] args) {
+        System.out.println("age - 30");
+    }
+}
